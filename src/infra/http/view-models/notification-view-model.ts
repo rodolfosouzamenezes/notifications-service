7dfebcceddd887fe850eb20ff56cf38f2d2ffba0
RodolfoSouzaMenezes <rodolfosouzaofc@gmail.com>
2022-12-15 17:37:28 -0300

    feat: create view model to http

---
+++ src/infra/http/view-models/notification-view-model.ts
@@ -0,0 +1,12 @@
+import { Notification } from '@application/entities/notification';
+
+export class NotificationViewModel {
+  static toHTTP(notification: Notification) {
+    return {
+      id: notification.id,
+      recipientId: notification.recipientId,
+      category: notification.category,
+      content: notification.content.value,
+    };
+  }
+}
