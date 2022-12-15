cd00e62598f845b537bd0690185d51630d7342e0
RodolfoSouzaMenezes <rodolfosouzaofc@gmail.com>
2022-12-15 20:33:14 -0300

    feat: cancel notification use case created

--- src/application/use-cases/send-notification.spec.ts
+++ src/application/use-cases/send-notification.spec.ts
@@ -1,4 +1,4 @@
-import { InMemoryNotificationsRepository } from '../../../test/repositories/in-memory-notificatons-repository';
+import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notificatons-repository';
 import { SendNotification } from './send-notification';
 
 describe('Send Notification', () => {
